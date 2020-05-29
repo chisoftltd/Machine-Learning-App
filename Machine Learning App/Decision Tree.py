b74aa4c0ed8477c313e3f3871dc6d9e5c9dd6850
chisoftltd <chisoftltd@gmail.com>
2020-05-29 17:32:01 +0200

    Machine Learning - Decision Tree

--- Machine Learning App/Decision Tree.py
+++ Machine Learning App/Decision Tree.py
@@ -17,3 +17,29 @@ d = {'YES': 1, 'NO': 0}
 df['Go'] = df['Go'].map(d)
 
 print(df)
+
+
+df = pandas.read_csv("shows.csv")
+
+d = {'UK': 0, 'USA': 1, 'N': 2}
+df['Nationality'] = df['Nationality'].map(d)
+d = {'YES': 1, 'NO': 0}
+df['Go'] = df['Go'].map(d)
+
+
+features = ['Age', 'Experience', 'Rank', 'Nationality']
+
+X = df[features]
+y = df['Go']
+
+print(X)
+print(y)
+
+dtree = dtree.fit(X, y)
+data = tree.export_graphviz(dtree, out_file=None, feature_names=features)
+graph = pydotplus.graph_from_dot_data(data)
+graph.write_png('mydecisiontree.png')
+
+img=pltimg.imread('mydecisiontree.png')
+imgplot = plt.imshow(img)
+plt.show()
